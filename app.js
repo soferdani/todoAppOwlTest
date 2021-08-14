61527a1b40593775dca81eca4c1240fd6eaf4176
soferdani <soferdani@gmail.com>
2021-08-14 20:37:48 +0300

    full functional app

--- app.js
+++ app.js
@@ -1,93 +1,125 @@
-const { Component, Store, mount } = owl;
-const { xml } = owl.tags;
-const { whenReady } = owl.utils;
-const { useRef, useDispatch, useStore } = owl.hooks;
+(function () {
+	const { Component, Store, mount } = owl;
+	const { xml } = owl.tags;
+	const { whenReady } = owl.utils;
+	const { useRef, useDispatch, useState, useStore } = owl.hooks;
 
-const actions = {
-	addTask({ state }, title) {
-		title = title.trim();
-		if (title) {
-			const task = {
-				id: state.nextId++,
-				title: title,
-				isCompleted: false,
-			};
-			state.tasks.push(task);
-		}
-	},
-	toggleTask({ state }, id) {
-		const task = state.tasks.find((t) => t.id === id);
-		task.isCompleted = !task.isCompleted;
-	},
-	deleteTask({ state }, id) {
-		const index = state.tasks.findIndex((t) => t.id === id);
-		state.tasks.splice(index, 1);
-	},
-};
-const initialState = {
-	nextId: 1,
-	tasks: [],
-};
+	const actions = {
+		addTask({ state }, title) {
+			title = title.trim();
+			if (title) {
+				const task = {
+					id: state.nextId++,
+					title: title,
+					isCompleted: false,
+				};
+				state.tasks.push(task);
+			}
+		},
+		toggleTask({ state }, id) {
+			const task = state.tasks.find((t) => t.id === id);
+			task.isCompleted = !task.isCompleted;
+		},
+		deleteTask({ state }, id) {
+			const index = state.tasks.findIndex((t) => t.id === id);
+			state.tasks.splice(index, 1);
+		},
+	};
 
-const TASK_TEMPLATE = xml/* xml */ `
-    <div class="task" t-att-class="props.task.isCompleted ? 'done' : ''">
-        <input type="checkbox" t-att-checked="props.task.isCompleted"
-                t-on-click="dispatch('toggleTask', props.task.id)"/>
-        <span><t t-esc="props.task.title"/></span>
-        <span class="delete" t-on-click="dispatch('deleteTask', props.task.id)">🗑</span>
-    </div>`;
+	const initialState = {
+		nextId: 1,
+		tasks: [],
+	};
 
-class Task extends Component {
-	static template = TASK_TEMPLATE;
-	static props = ["task"];
-	dispatch = useDispatch();
-}
+	const TASK_TEMPLATE = xml/* xml */ `
+      <div class="task" t-att-class="props.task.isCompleted ? 'done' : ''">
+          <input type="checkbox" t-att-checked="props.task.isCompleted"
+              t-att-id="props.task.id"
+              t-on-click="dispatch('toggleTask', props.task.id)"/>
+          <label t-att-for="props.task.id"><t t-esc="props.task.title"/></label>
+          <span class="delete" t-on-click="dispatch('deleteTask', props.task.id)">🗑</span>
+      </div>`;
 
-const APP_TEMPLATE = xml/* xml */ `
-    <div class="todo-app">
-        <input placeholder="Enter a new task" t-on-keyup="addTask" t-ref="add-input"/>
-        <div class="task-list">
-            <t t-foreach="tasks" t-as="task" t-key="task.id">
-                <Task task="task"/>
-            </t>
-        </div>
-    </div>`;
-
-class App extends Component {
-	static template = APP_TEMPLATE;
-	static components = { Task };
-
-	inputRef = useRef("add-input");
-	tasks = useStore((state) => state.tasks);
-	dispatch = useDispatch();
-
-	mounted() {
-		this.inputRef.el.focus();
+	class Task extends Component {
+		static template = TASK_TEMPLATE;
+		static props = ["task"];
+		dispatch = useDispatch();
 	}
 
-	addTask(ev) {
-		if (ev.keyCode === 13) {
-			this.dispatch("addTask", ev.target.value);
-			ev.target.value = "";
+	const APP_TEMPLATE = xml/* xml */ `
+      <div class="todo-app">
+          <input placeholder="Enter a new task" t-on-keyup="addTask" t-ref="add-input"/>
+          <div class="task-list">
+              <Task t-foreach="displayedTasks" t-as="task" t-key="task.id" task="task"/>
+          </div>
+          <div class="task-panel" t-if="tasks.length">
+              <div class="task-counter">
+                  <t t-esc="displayedTasks.length"/>
+                  <t t-if="displayedTasks.length lt tasks.length">
+                      / <t t-esc="tasks.length"/>
+                  </t>
+                  task(s)
+              </div>
+              <div>
+                  <span t-foreach="['all', 'active', 'completed']"
+                      t-as="f" t-key="f"
+                      t-att-class="{active: filter.value===f}"
+                      t-on-click="setFilter(f)"
+                      t-esc="f"/>
+              </div>
+          </div>
+      </div>`;
+
+	class App extends Component {
+		static template = APP_TEMPLATE;
+		static components = { Task };
+
+		inputRef = useRef("add-input");
+		tasks = useStore((state) => state.tasks);
+		filter = useState({ value: "all" });
+		dispatch = useDispatch();
+
+		mounted() {
+			this.inputRef.el.focus();
+		}
+
+		addTask(ev) {
+			if (ev.keyCode === 13) {
+				this.dispatch("addTask", ev.target.value);
+				ev.target.value = "";
+			}
+		}
+
+		get displayedTasks() {
+			switch (this.filter.value) {
+				case "active":
+					return this.tasks.filter((t) => !t.isCompleted);
+				case "completed":
+					return this.tasks.filter((t) => t.isCompleted);
+				case "all":
+					return this.tasks;
+			}
+		}
+		setFilter(filter) {
+			this.filter.value = filter;
 		}
 	}
-}
 
-function makeStore() {
-	const localState = window.localStorage.getItem("todoapp");
-	const state = localState ? JSON.parse(localState) : initialState;
-	const store = new Store({ state, actions });
-	store.on("update", null, () => {
-		localStorage.setItem("todoapp", JSON.stringify(store.state));
-	});
-	return store;
-}
+	function makeStore() {
+		const localState = window.localStorage.getItem("todoapp");
+		const state = localState ? JSON.parse(localState) : initialState;
+		const store = new Store({ state, actions });
+		store.on("update", null, () => {
+			localStorage.setItem("todoapp", JSON.stringify(store.state));
+		});
+		return store;
+	}
 
-function setup() {
-	owl.config.mode = "dev";
-	const store = new Store({ actions, state: initialState });
-	const env = { store: makeStore() };
-	mount(App, { target: document.body, env });
-}
+	function setup() {
+		owl.config.mode = "dev";
+		const env = { store: makeStore() };
+		mount(App, { target: document.body, env });
+	}
 
-whenReady(setup);
+	whenReady(setup);
+})();
