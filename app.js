375dba8c12dd106729ab43d2abaf48cc26c025ca
soferdani <soferdani@gmail.com>
2021-08-14 20:33:08 +0300

    part 11 is ready

--- app.js
+++ app.js
@@ -1,82 +1,93 @@
-const { Component, mount } = owl;
+const { Component, Store, mount } = owl;
 const { xml } = owl.tags;
 const { whenReady } = owl.utils;
-const { useRef, useState } = owl.hooks;
+const { useRef, useDispatch, useStore } = owl.hooks;
+
+const actions = {
+	addTask({ state }, title) {
+		title = title.trim();
+		if (title) {
+			const task = {
+				id: state.nextId++,
+				title: title,
+				isCompleted: false,
+			};
+			state.tasks.push(task);
+		}
+	},
+	toggleTask({ state }, id) {
+		const task = state.tasks.find((t) => t.id === id);
+		task.isCompleted = !task.isCompleted;
+	},
+	deleteTask({ state }, id) {
+		const index = state.tasks.findIndex((t) => t.id === id);
+		state.tasks.splice(index, 1);
+	},
+};
+const initialState = {
+	nextId: 1,
+	tasks: [],
+};
 
 const TASK_TEMPLATE = xml/* xml */ `
-<div class="task" t-att-class="props.task.isCompleted ? 'done' : ''">
-    <input type="checkbox" t-att-checked="props.task.isCompleted" t-on-click="toggleTask"/>
-    <span><t t-esc="props.task.title"/></span>
-    <span class="delete" t-on-click="deleteTask">🗑</span>
-</div>`;
+    <div class="task" t-att-class="props.task.isCompleted ? 'done' : ''">
+        <input type="checkbox" t-att-checked="props.task.isCompleted"
+                t-on-click="dispatch('toggleTask', props.task.id)"/>
+        <span><t t-esc="props.task.title"/></span>
+        <span class="delete" t-on-click="dispatch('deleteTask', props.task.id)">🗑</span>
+    </div>`;
 
 class Task extends Component {
 	static template = TASK_TEMPLATE;
 	static props = ["task"];
-
-	toggleTask() {
-		this.trigger("toggle-task", { id: this.props.task.id });
-    }
-    
-    deleteTask() {
-        this.trigger('delete-task', {id: this.props.task.id});
-    }
+	dispatch = useDispatch();
 }
 
 const APP_TEMPLATE = xml/* xml */ `
-<div class="todo-app">
-<input placeholder="Enter a new task" t-on-keyup="addTask" t-ref="add-input" />
-<div class="task-list" t-on-toggle-task="toggleTask" t-on-delete-task="deleteTask" >
-    <t t-foreach="tasks" t-as="task" t-key="task.id">
-        <Task task="task"/>
-    </t>
-</div>
-</div>`;
+    <div class="todo-app">
+        <input placeholder="Enter a new task" t-on-keyup="addTask" t-ref="add-input"/>
+        <div class="task-list">
+            <t t-foreach="tasks" t-as="task" t-key="task.id">
+                <Task task="task"/>
+            </t>
+        </div>
+    </div>`;
 
 class App extends Component {
 	static template = APP_TEMPLATE;
 	static components = { Task };
 
 	inputRef = useRef("add-input");
-
-	nextId = 1;
-	tasks = useState([]);
+	tasks = useStore((state) => state.tasks);
+	dispatch = useDispatch();
 
 	mounted() {
 		this.inputRef.el.focus();
 	}
 
 	addTask(ev) {
-		// 13 is keycode for ENTER
 		if (ev.keyCode === 13) {
-			const title = ev.target.value.trim();
+			this.dispatch("addTask", ev.target.value);
 			ev.target.value = "";
-
-			if (title) {
-				const newTask = {
-					id: this.nextId++,
-					title: title,
-					isCompleted: false,
-				};
-				this.tasks.push(newTask);
-			}
 		}
 	}
+}
 
-	toggleTask(ev) {
-		const task = this.tasks.find((t) => t.id === ev.detail.id);
-		task.isCompleted = !task.isCompleted;
-    }
-    
-    deleteTask(ev) {
-        const index = this.tasks.findIndex(t => t.id === ev.detail.id);
-        this.tasks.splice(index, 1);
-    }
+function makeStore() {
+	const localState = window.localStorage.getItem("todoapp");
+	const state = localState ? JSON.parse(localState) : initialState;
+	const store = new Store({ state, actions });
+	store.on("update", null, () => {
+		localStorage.setItem("todoapp", JSON.stringify(store.state));
+	});
+	return store;
 }
 
 function setup() {
 	owl.config.mode = "dev";
-	mount(App, { target: document.body });
+	const store = new Store({ actions, state: initialState });
+	const env = { store: makeStore() };
+	mount(App, { target: document.body, env });
 }
 
 whenReady(setup);
