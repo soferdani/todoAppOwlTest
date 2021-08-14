8ac0602f3839183868bc15c8e1cf78812290c13a
soferdani <soferdani@gmail.com>
2021-08-14 20:16:26 +0300

    part 7 is ready

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const { Component, mount } = owl;
 const { xml } = owl.tags;
 const { whenReady } = owl.utils;
-const { useRef } = owl.hooks;
-
+const { useRef, useState } = owl.hooks;
 
 const TASK_TEMPLATE = xml/* xml */ `
 <div class="task" t-att-class="props.task.isCompleted ? 'done' : ''">
@@ -17,7 +16,7 @@ class Task extends Component {
 
 const APP_TEMPLATE = xml/* xml */ `
 <div class="todo-app">
-<input placeholder="Enter a new task" t-on-keyup="addTask"/>
+<input placeholder="Enter a new task" t-on-keyup="addTask" t-ref="add-input" />
 <div class="task-list">
     <t t-foreach="tasks" t-as="task" t-key="task.id">
         <Task task="task"/>
@@ -29,33 +28,32 @@ class App extends Component {
 	static template = APP_TEMPLATE;
 	static components = { Task };
 
+	inputRef = useRef("add-input");
 
+	nextId = 1;
+	tasks = useState([]);
 
-    addTask(ev) {
-        // 13 is keycode for ENTER
-        if (ev.keyCode === 13) {
-            const title = ev.target.value.trim();
-            ev.target.value = "";
-            console.log('adding task', title);
-            // todo
-        }
-    }
+	mounted() {
+		this.inputRef.el.focus();
+	}
 
+	addTask(ev) {
+		// 13 is keycode for ENTER
+		if (ev.keyCode === 13) {
+			const title = ev.target.value.trim();
+			ev.target.value = "";
 
+			if (title) {
+				const newTask = {
+					id: this.nextId++,
+					title: title,
+					isCompleted: false,
+				};
+				this.tasks.push(newTask);
+			}
+		}
+	}
 
-
-	tasks = [
-		{
-			id: 1,
-			title: "buy milk",
-			isCompleted: true,
-		},
-		{
-			id: 2,
-			title: "clean house",
-			isCompleted: false,
-		},
-	];
 }
 
 function setup() {
